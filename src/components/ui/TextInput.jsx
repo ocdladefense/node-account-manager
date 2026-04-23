a68ba6629c740cf119e9542a46accd2fbbb470ec
Rosa Myers <larosaardente@gmail.com>
2026-04-22 18:30:36 -0700

    Added code to convert TextInput's label parameter to an id-friendly value.

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,8 +1,9 @@
 export default function TextInput({ label, apiName, currentValue }) {
+    const id = label.split(" ").filter((t) => t).join("");
     return (
         <div className="mb-4">
-            <label htmlFor={label} className="block text-sm font-semibold mb-2">{label}</label>
-            <input type="text" name={apiName} defaultValue={currentValue} id={label} className="w-full px-3 py-2 border rounded" />
+            <label htmlFor={id} className="block text-sm font-semibold mb-2">{label}</label>
+            <input type="text" name={apiName} defaultValue={currentValue} id={id} className="w-full px-3 py-2 border rounded" />
         </div>
     )
 }
