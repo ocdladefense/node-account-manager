0def5d62e177598ab8256279a5c1778d806fcb31
Jason Lewis <lewisjc@my.lanecc.edu>
2026-05-01 13:54:25 -0700

    Moved form components into form subfolder. Fixed import links on contact form

--- src/components/contacts/ContactForm.jsx
+++ src/components/contacts/ContactForm.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useOutletContext } from "react-router-dom";
 import { getContactQuery } from "./query.js";
-import PickList from "../ui/PickList.jsx";
-import TextInput from "../ui/TextInput.jsx"
 import { Pi } from "lucide-react";
+import PickList from "../ui/form/PickList.jsx";
+import TextInput from "../ui/form/TextInput.jsx";
+import CheckBox from "../ui/form/Checkbox.jsx";
+
 
 export default function ContactForm() {
 
