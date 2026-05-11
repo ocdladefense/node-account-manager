28c6c0d074025187c32977504af44b11cff757b7
Rosa Myers <larosaardente@gmail.com>
2026-05-11 15:37:05 -0700

    Upgraded the menu bar to a dasiy drawer; added links hooked up to the existing components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -60,17 +60,44 @@ export default function App() {
     return (
         <div className="mx-auto">
             <Header loggedIn={isLoggedIn()} />
-            <div className="flex flex-row">
+            <div className="drawer md:drawer-open">
+                <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-3" className="btn drawer-button md:hidden text-sm w-20">
+                        Menu
+                    </label>
+                    {!appReady ? <h1>Loading...</h1> : <Outlet context={{ client }} />}
+
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                        {/* Sidebar content here */}
+                        <li><a href="/">Home</a></li>
+                        {/* Hardcoding ids for now. Presumably these will ultimately be gotten from the logged-in user's data (authentication orocess)*/}
+                        <li><a href='/account/001j000000oPGAmAAO'>Account</a></li>
+                        <li><a href='/contact/0030a00002HpppSAAR'>Profile</a></li>
+                        <li><a href='/account/001j000000oPGAmAAO/contacts'>Account Directory</a></li>
+                        <li><a href='/account/001j000000oPGAmAAO/orders'>Orders</a></li>
+                        {/* Currently a dummy link */}
+                        <li><a href='/account/001j000000oPGAmAAO/payments'>Upcoming Payments</a></li>
+                        <li><a href='/account/001j000000oPGAmAAO/events'>Seminars and Events</a></li>
+
+                    </ul>
+                </div>
+            </div>
+            {/* <div className="flex flex-row">
                 <nav className="hidden md:flex m-5 mt-25 border-t w-1/4">
                     <ul className="">
-                        <li><Link to="/" className="block text-blue-600 underline">Home</Link></li>
-                        <li><Link to="/accounts" className="block text-blue-600 underline">Accounts</Link></li>
+                        <li><Link to="/" className="block text-blue-600 underline"></Link></li>
+                        <li><Link to="/accounts" className="block text-blue-600 underline"></Link></li>
                     </ul>
                 </nav>
                 <div className="flex w-3/4">
-                    {!appReady ? <h1>Loading...</h1> : <Outlet context={{ client }} />}
+                    
                 </div>
-            </div>
+            </div> */}
             {/* <Footer /> */}
         </div>
     );
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -34,12 +34,10 @@ export default function Header({ loggedIn = false }) {
         // loggedIn: loggedIn
     };
 
-    if (loggedIn)
-    {
+    if (loggedIn) {
         console.log("User is logged in");
         items.push(logoutItem);
-    } else
-    {
+    } else {
         console.log("User is NOT logged in");
         items.push(loginItem);
     }
@@ -47,7 +45,7 @@ export default function Header({ loggedIn = false }) {
     // 
 
     return (
-        <header className="w-full mb-0 py-1 px-[10px] fixed top-0 z-50 bg-transparent">
+        <header className="w-full mb-0 py-1 px-[10px] sticky top-0 z-50 bg-transparent">
             <nav>
 
                 <ul className="inline-block" style={{ float: "right" }}>
