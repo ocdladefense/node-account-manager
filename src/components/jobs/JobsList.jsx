8c50de36183ce1c4f81a7fce314c6bea11443401
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-07-20 17:45:29 -0700

    Added a Jsdoc comment on the functionality for component JobsList.

--- src/components/jobs/JobsList.jsx
+++ src/components/jobs/JobsList.jsx
@@ -1,5 +1,11 @@
 import JobCard from "./JobCard.jsx";
 
+/**
+ * Renders a list of job postings using the JobCard component for each job.
+ * @param {Object} param - The parameters for the JobList component
+ * @param {Array} param.jobs - An array of job objects from Salesforce, each containing details like title, organization, location, and salary.
+ * @returns {React.JSX.Element} A list of job cards.
+ */
 export default function JobList({ jobs = [] }) {
     return (
         <div className="space-y-4">
