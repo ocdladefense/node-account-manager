4601e4559c10244e141561a90b0550d2ac9cf5c3
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-07-20 17:46:24 -0700

    Added a Jsdoc comment on the functionality for the model Job.

--- src/models/Job.js
+++ src/models/Job.js
@@ -1,3 +1,15 @@
+/**
+ * Represents a job posting object.
+ *
+ * @property {string} [id] - The unique ID of the job posting.
+ * @property {string} title - The title of the job.
+ * @property {string} [description] - Detailed job description.
+ * @property {number} postDate - Timestamp when the job was created.
+ * @property {string} [closeDate] - Application closing date.
+ * @property {string} [organization] - The organization or company name.
+ * @property {string} salary - The salary range or amount.
+ * @property {string} [documentName] - Name of the attached file/document.
+ */
 export default class Job {
     id;
 
@@ -15,6 +27,10 @@ export default class Job {
 
     documentName;
 
+    /**
+     * @param {string} title - The Job Title
+     * @param {string} salary - The job salary
+     */
     constructor(title, salary) {
         this.title = title;
         this.salary = salary;
