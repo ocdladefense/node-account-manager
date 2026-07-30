c373f3d54e79f5e81de09d9f44d4f44de7e4bede
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-07-30 13:28:21 -0700

    Removed the buttonstyle as a default option and instead moved it down to className.

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,11 +1,11 @@
 import "../../css/contact.css"
-export default function Button({ action, label, className = "buttonStyle", buttonType = "button" }) {
+export default function Button({ action, label, className = " ", buttonType = "button" }) {
     return (
         <>
             <button
                 type={buttonType}
                 onClick={action}
-                className={className}
+                className={`buttonStyle ${className}`}
 
             >
                 {label}
