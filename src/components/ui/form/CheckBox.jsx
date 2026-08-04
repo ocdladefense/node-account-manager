7c07406cb0ba5f66bb7f373131e672650e1eb9e5
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-08-03 21:50:31 -0700

    added onChange

--- src/components/ui/form/CheckBox.jsx
+++ src/components/ui/form/CheckBox.jsx
@@ -1,4 +1,4 @@
-export default function CheckBox({ label, name, defaultValue }) {
+export default function CheckBox({ label, name, defaultValue, onChange }) {
     return (
         <div>
             <label className="block text-sm font-semibold mb-2" htmlFor={label}>
@@ -8,6 +8,7 @@ export default function CheckBox({ label, name, defaultValue }) {
                     className="checkbox ml-3"
                     name={name}
                     defaultChecked={defaultValue}
+                    onChange={onChange}
                 />
             </label>
         </div>
