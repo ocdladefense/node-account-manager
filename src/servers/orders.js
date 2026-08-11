682ac3260a484a9b8adc54ce97a5a54e4a132ad7
José Bernal <jbernal.web.dev@gmail.com>
2026-08-10 18:08:21 -0700

    Add an order.

--- src/servers/orders.js
+++ src/servers/orders.js
@@ -19,7 +19,7 @@ const BASE_UPLOADS_DIR = path.resolve("uploads");
  */
 router.post("/orders", async (req, res) => {
 
-    const pricebookEntryId = "01u0a00000Hb09A"; // req.body.productId;;
+    const product2Id = req.body.productId;
     const contactIds = ["003hr000000NvVR"];//req.body.contactIds;
 
     let url = req.cookies.instance_url;
@@ -28,12 +28,34 @@ router.post("/orders", async (req, res) => {
     let client = new SalesforceRestApi(url, token);
     let orderRecord = { Name: "Foobar 2", AccountId: req.cookies.account_id, EffectiveDate: new Date().toISOString().split('T')[0], Status: 'Draft' };
 
+    let pricebookIdResp = await client.query(`SELECT Id FROM Pricebook2 WHERE IsActive = true AND IsStandard = true LIMIT 1`);
+    console.log("orders.js: pricebookIdResp: ", pricebookIdResp);
+    // let json = await pricebookIdResp.json();
+    let pricebookId = pricebookIdResp.records[0].Id;
+    orderRecord.Pricebook2Id = pricebookId;
+
+    let pricebookEntryResp = await client.query(`SELECT Id FROM PricebookEntry WHERE Product2Id = '${product2Id}' AND IsActive = true LIMIT 1`);
+    console.log("orders.js: pricebookEntryResp: ", pricebookEntryResp);
+    // let json = await pricebookEntryResp.json();
+    let pricebookEntryId = pricebookEntryResp.records[0].Id;
+
     // Next step: insert an order product using the required data, OrderId.
 
     let resp = await client.create('Order', orderRecord);
-    console.log(await resp.json()); // Should return the id of the newly-created order record.
+    let jsonResp = await resp.json(); // Should return the id of the newly-created order record.
 
-    res.json(resp);
+
+    let orderId = jsonResp.id;
+
+    for (let contactId of contactIds)
+    {
+        let orderProductRecord = { OrderId: orderId, Contact__c: contactId, PricebookEntryId: pricebookEntryId, Quantity: 1, UnitPrice: 100.00 };
+        let orderProductResp = await client.create('OrderItem', orderProductRecord);
+        let orderProductJsonResp = await orderProductResp.json();
+        console.log("orders.js: created order product record: ", orderProductJsonResp);
+    }
+
+    res.json(jsonResp);
 });
 
 export default router;
