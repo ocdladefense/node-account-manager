d0ee07d9b2f4651b74d3f997ae85bf0075cd8c5a
José Bernal <jbernal.web.dev@gmail.com>
2026-08-12 15:50:56 -0700

    Use a modal.

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -3,9 +3,9 @@ import { useEffect, useRef } from 'react';
 
 
 
-export default function Modal({ isOpen, onClose, confirmAction, content, externalNode }) {
-    if (!isOpen) return null;
-    let title = "MY TOC";
+export default function Modal({ isOpen, onClose, confirmAction, content, externalNode, defaultButtons = true }) {
+    // if (!isOpen) return null; // does this "early return" create an issue in some implementations??
+
     const containerRef = useRef(null);
 
 
@@ -26,13 +26,13 @@ export default function Modal({ isOpen, onClose, confirmAction, content, externa
         };
     }, [externalNode]); // Rerun if the external node changes
 
-    return ReactDOM.createPortal(
-        <div style={{ zIndex: 100, top: "0px", left: "0px", textAlign: "center", height: "100vh", width: "100vw" }} onClick={onClose} className="modal fixed p-8 bg-black bg-opacity-70">
+    return isOpen && ReactDOM.createPortal(
+        <div id="my-modal" style={{ zIndex: 100, top: "0px", left: "0px", textAlign: "center", height: "100vh", width: "100vw" }} onClick={onClose} className="fixed p-8 bg-black bg-opacity-70">
             <div style={{ height: "90%", margin: "0 auto", overflowY: "scroll", overflowX: "hidden" }} className="bg-white rounded-xl p-6 shadow-xl" onClick={(e) => e.stopPropagation()}>
                 {content}
                 <div ref={containerRef} />
                 <div>
-                    <button onClick={onClose} className="bg-gray-300 px-4 py-2 rounded">Cancel</button>
+                    {defaultButtons !== false && <button onClick={onClose} className="bg-gray-300 px-4 py-2 rounded">Cancel</button>}
                     {/*<button onClick={confirmAction} className="bg-green-600 text-white px-4 py-2 rounded">Confirm</button>*/}
                 </div>
             </div>
