ecb2a98c8624805be5f457c0cc98b920fb3ae12b
Jacob Dykstra <OcularRiver57@gmail.com>
2026-08-18 12:56:08 -0700

    added standard toast object

---
+++ src/components/ui/notifications/Toast.jsx
@@ -0,0 +1,19 @@
+/**
+ * provides a standard toast object to be used with CreateToast, not to be used standalone.
+ * @param {string} Message the message the toast will display to the user
+ * @param {string} [className] the className to be passed to the div for styling
+ * @param {boolean} [useDefaultSyle=true] if the toast should use the default style in addition to user provided style
+ * @returns {React.JSX.Element} Toast
+ */
+export function Toast (Message, className="", useDefaultSyle=true){
+    let styles;
+    if (useDefaultSyle) styles = "bg-green-500 text-black px-6 py-4 text-lg font-semibold rounded-lg shadow-lg" + className;
+    else styles = className;
+
+    return (
+        <div className="{styles}">
+            {Message}
+        </div>
+    );
+}
+
