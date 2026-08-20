c3519b6f5e9526a260aac77dae2b9741aa5e7428
José Bernal <jbernal.web.dev@gmail.com>
2026-08-20 15:26:07 -0700

    Prepare homepage layout.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -17,8 +17,65 @@ export default function HomePage() {
 
                 <div className="space-y-2">
                     <p>Here's where we explain our purpose to you!</p>
-                    <br />
-                    {userId ? <a href="/logout">Log Out</a> : <a href="/login">Log In</a>}
+                </div>
+
+                <h1 className="mt-12 text-2xl font-bold mb-4">Upcoming Events</h1>
+
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-8">
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Medium Card</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Medium Card</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Medium Card</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h1 className="mt-8 text-2xl font-bold mb-4">Add to your membership</h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-8">
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Medium Card</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card w-96 bg-base-100 card-md shadow-sm">
+                        <div className="card-body">
+                            <h2 className="card-title">Medium Card</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="justify-end card-actions">
+                                <button className="btn btn-primary">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
