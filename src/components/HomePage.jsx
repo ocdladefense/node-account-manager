df73bc2f8c4bbdde6bcbea5c859a8e0c426692a5
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-08-24 23:18:32 -0700

    set up the card component on Homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { useOutletContext } from "react-router-dom";
 import { getAccountContactsQuery } from './accounts/query';
 import { getCookie } from '@ocdla/salesforce/CookieUtils';
+import Card from './ui/Card';
 
 export default function HomePage() {
 
@@ -11,8 +12,7 @@ export default function HomePage() {
 
 
     return (
-        <div className="w-full">
-            <div className="container mx-auto px-2 mt-[28px]">
+        <div className="w-full px-8 py-6">
                 <h1 className="text-2xl font-bold mb-4">Welcome to OCDLA!</h1>
 
                 <div className="space-y-2">
@@ -22,62 +22,43 @@ export default function HomePage() {
                 <h1 className="mt-12 text-2xl font-bold mb-4">Upcoming Events</h1>
 
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-8">
-                    <div className="card w-96 bg-base-100 card-md shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">Medium Card</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div className="justify-end card-actions">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
-                        </div>
-                    </div>
+            <div className="flex flex-wrap gap-6 mt-6">
 
-                    <div className="card w-96 bg-base-100 card-md shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">Medium Card</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div className="justify-end card-actions">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
-                        </div>
-                    </div>
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Register</button>}
+                />
 
-                    <div className="card w-96 bg-base-100 card-md shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">Medium Card</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div className="justify-end card-actions">
-                                <button className="btn btn-primary">Register</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Register</button>}
+                />
 
-                <h1 className="mt-8 text-2xl font-bold mb-4">Add to your membership</h1>
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Register</button>}
+                />
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-8">
-                    <div className="card w-96 bg-base-100 card-md shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">Medium Card</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div className="justify-end card-actions">
-                                <button className="btn btn-primary">Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Register</button>}
+                />
+            </div>
 
-                    <div className="card w-96 bg-base-100 card-md shadow-sm">
-                        <div className="card-body">
-                            <h2 className="card-title">Medium Card</h2>
-                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                            <div className="justify-end card-actions">
-                                <button className="btn btn-primary">Subscribe</button>
-                            </div>
-                        </div>
-                    </div>
+            <h1 className="mt-10 text-2xl font-bold mb-4">Add to your membership</h1>
 
-                </div>
+            <div className="flex flex-wrap gap-6 mt-6">
+
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Subscribe</button>}
+                />
+
+                <Card
+                    title="Medium Card" description="A card component has a figure, a body part, and inside body there are title and actions parts"
+                    actions={<button className="btn btn-primary">Subscribe</button>}
+                />
+
             </div>
         </div>
     )
