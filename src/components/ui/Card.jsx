696ed00527f8f809fb0247b59310efe314f6eee3
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-08-25 13:28:55 -0700

    Changes to card which handle children.

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,11 +1,12 @@
-export default function Card({ title, description, actions, className = '' }) {
+export default function Card({ title, description, children, actions, className = '' }) {
 
     return (
         <div className={`card bg-base-100 card-md shadow-sm w-96 ${className}`}>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>{description}</p>
-                <div className="justify-end card-actions">{actions}</div>
+                <div className="justify-end card-actions">{children || <button className="btn btn-primary">Go</button>}</div>
+
             </div>
         </div>
     );
