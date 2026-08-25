25504b0c55c174acb5473a14942fc5e0bf057d86
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-08-24 23:17:37 -0700

    Card component for subscribe and register for events on the home page.

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card({ title, description, actions, className = '' }) {
+
+    return (
+        <div className={`card bg-base-100 card-md shadow-sm w-96 ${className}`}>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="justify-end card-actions">{actions}</div>
+            </div>
+        </div>
+    );
+}
