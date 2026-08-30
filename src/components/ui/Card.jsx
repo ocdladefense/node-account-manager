d947b73e4e2a8def22f1bc49277e7fbf49db5950
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-08-29 22:41:35 -0700

    added an ifButton option in case the card shoudld have no button.

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,11 +1,14 @@
-export default function Card({ title, description, children, actions, className = '' }) {
+export default function Card({ title, description, children, actions, className = '', ifButton = true }) {
 
     return (
         <div className={`card bg-base-100 card-md shadow-sm w-96 ${className}`}>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>{description}</p>
-                <div className="justify-end card-actions">{children || <button className="btn btn-primary">Go</button>}</div>
+
+                {ifButton &&
+                    <div className="justify-end card-actions">{children || <button className="btn btn-primary">Go</button>}</div>
+                }
 
             </div>
         </div>
