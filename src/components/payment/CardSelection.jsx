09d63a4c7476bcb4ea5c99e75e5784bd495d91c7
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-09-05 11:24:32 -0700

    Now takes a use effect for payment options

--- src/components/payment/CardSelection.jsx
+++ src/components/payment/CardSelection.jsx
@@ -1,16 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DropMenu from "../ui/form/DropMenu";
 import InputForm from "./InputForm";
 
-const PAYMENT_OPTIONS = [
-    { Id: "card_1234", Name: "Visa ending in 1234", type: "saved_card" },
-    { Id: "card_5678", Name: "Mastercard ending in 5678", type: "saved_card" },
+
+const defaultPaymentOption = [
     { Id: "new_card", Name: "+ Pay with New Card", type: "new_card" },
     { Id: "invoice", Name: "Bill via Invoice", type: "invoice" },
 ];
 
 export default function CardSelection() {
     const [selectedOption, setSelectedOption] = useState(null);
+    const [paymentOptions, setPaymentOptions] = useState(defaultPaymentOption);
+
+    useEffect(() => {
+        const fetchPaymentMethods = async () => {
+            try {
+                // Implement later
+                // const resp = await fetch("/api/query/event-products?eventId=" + selectedEvent?.Id);
+                // const data = await resp.json();
+
+                let data = {
+                    records: [
+                        { Id: "card_1234", Name: "Visa ending in 1234", type: "saved_card" },
+                        { Id: "card_5678", Name: "Mastercard ending in 5678", type: "saved_card" }
+                    ]
+                }
+
+                // if (!resp.ok) {
+                //     throw new Error(
+                //         data.error || "Unable to retrieve event products."
+                //     );
+                // }
+
+                setPaymentOptions((options) => {
+                    return [...data.records, ...options];
+                });
+
+            } catch (error) {
+                console.error(
+                    "Error fetching payment method",
+                    error
+                );
+            }
+        };
+
+        fetchPaymentMethods();
+    }, []); // Should we rerun fetch payment methods if customer selects new payment?
+
+
+
+
 
     return (
         <div className="w-full flex flex-col items-center space-y-4">
@@ -23,7 +62,7 @@ export default function CardSelection() {
 
             <DropMenu
                 label={selectedOption ? selectedOption.Name : "Choose a card"}
-                entries={PAYMENT_OPTIONS}
+                entries={paymentOptions}
                 handler={(option) => setSelectedOption(option)}
                 thingThatGetsDisplayed={(option) => option.Name}
             />
