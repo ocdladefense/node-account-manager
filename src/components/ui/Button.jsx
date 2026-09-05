14f462aa9ff5a8e300c6e694bab7d1ee002ad813
Jacob Dykstra <OcularRiver57@gmail.com>
2026-09-05 12:13:46 -0700

    caution button now takes size param like normal button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -12,7 +12,7 @@ export default function Button({ label, size = "px-6 py-2 w-50", className = "",
 
 }
 
-export function CautionButton({ label, className, buttonType = "button", isCancel = false, action }) {
+export function CautionButton({ label, size = "px-6 w-50 py-2 mx-2.5", className, buttonType = "button", isCancel = false, action }) {
 
     const variantColor = isCancel ? "bg-yellow-500 cursor-pointer hover:bg-yellow-600" : "bg-red-500 hover:bg-red-600";
 
@@ -20,7 +20,7 @@ export function CautionButton({ label, className, buttonType = "button", isCance
         <button
             type={buttonType}
             onClick={action}
-            className={className || `px-6 w-50 py-2 cursor-pointer rounded-sm mx-2.5 text-white ${variantColor}`}
+            className={className || `cursor-pointer rounded-sm mx-2.5 text-white ${variantColor} ${size}`}
         >
             {label}
         </button>
