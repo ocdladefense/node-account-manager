d31b0087b99fbd70ff01986b5e0f92055c1f3180
Jacob Dykstra <OcularRiver57@gmail.com>
2026-09-05 12:46:19 -0700

    removed redundant style on button

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -32,7 +32,7 @@ export function BackButton({ label, className = " ", buttonType = "button", acti
         <button
             type={buttonType}
             onClick={action}
-            className={className || "px-6 w-25 py-1 cursor-pointer rounded-md bg-white text-black cursor-pointer border border-black"}
+            className={className || "px-6 w-25 py-1 rounded-md bg-white text-black cursor-pointer border border-black"}
 
         >
             {label}
